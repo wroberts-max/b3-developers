0388a6c213c5241cf40b88a656781d00ecebc0d7
wroberts-max <234008602+wroberts-max@users.noreply.github.com>
2025-09-29 12:07:19 -0500

    add sitemap files to out folder

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,4 +3,5 @@ module.exports = {
   siteUrl: 'https://b3devs.com', // change to your live domain
   generateRobotsTxt: true,       // will create robots.txt too
   sitemapSize: 7000,
+  outDir: './out',               // <- write straight into the deploy folder
 };
