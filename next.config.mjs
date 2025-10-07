f393214a2c931eb8734bae73185954e0b9269ce3
wroberts-max <234008602+wroberts-max@users.noreply.github.com>
2025-10-07 16:19:50 -0500

    hero update

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  images: { unoptimized: true },
-  trailingSlash: true,
-  // 👇 change this to your actual repo name EXACTLY as it appears on GitHub
-  basePath: "/<b3-developers>",
-  assetPrefix: "/<b3-developers>/",
+  output: "export",                 //enables static export
+  images: { unoptimized: true },    // required for GitHub Pages
+  trailingSlash: true,              // helps avoid routing issues
 };
 
 export default nextConfig;
